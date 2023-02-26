966a343917787cc34999ba2913bf9a41dc219877
yahomi-dev <yahomi.dev@gmail.com>
2023-02-26 14:17:42 +0900

    format on saveの設定を追加

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -3,6 +3,7 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local null_ls = require('null-ls')
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
     -- vscodeでの設定との差異があるので直接指定
     local prettier_args = {
@@ -15,11 +16,24 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.prettier.with({
-          filetypes = { 'javascript' },
+          filetypes = { 'javascript', 'lua' },
           extra_args = prettier_args
         }),
       },
       command = 'prettier',
+      debug = false,
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end
+          })
+        end
+      end
     })
 
     vim.api.nvim_set_keymap('n', '<leader>fm', '<cmd>lua vim.lsp.buf.format()<CR>',
