3a9216a8e7bb4b8c7c8ad94217dfd6635bc875cb
yahomi-dev <yahomi.dev@gmail.com>
2024-08-19 11:22:22 +0900

    scrollbar一旦廃止

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -329,30 +329,30 @@ return {
   --   end,
   -- },
 
-  {
-    'petertriho/nvim-scrollbar',
-    config = function()
-      local scrollbar = require('scrollbar')
-      scrollbar.setup {
-        show_in_active_only = true,
-        handle = {
-          blend = 0,
-          text = ' ',
-          color = '#3b3a40',
-          color_nr = 234,
-        },
-        marks = {
-          Search = { color = '#C9A554' },
-          Error = { color = '#685742' },
-          Warn = { color = '#B36D43' },
-          Info = { color = '#5f875f' },
-          Hint = { color = '#5f875f' },
-          Misc = { color = '#bb7744' },
-          Cursor = { color = '#666666', text = ' ' },
-        },
-      }
-    end,
-  },
+  -- {
+  --   'petertriho/nvim-scrollbar',
+  --   config = function()
+  --     local scrollbar = require('scrollbar')
+  --     scrollbar.setup {
+  --       show_in_active_only = true,
+  --       handle = {
+  --         blend = 0,
+  --         text = ' ',
+  --         color = '#3b3a40',
+  --         color_nr = 234,
+  --       },
+  --       marks = {
+  --         Search = { color = '#C9A554' },
+  --         Error = { color = '#685742' },
+  --         Warn = { color = '#B36D43' },
+  --         Info = { color = '#5f875f' },
+  --         Hint = { color = '#5f875f' },
+  --         Misc = { color = '#bb7744' },
+  --         Cursor = { color = '#666666', text = ' ' },
+  --       },
+  --     }
+  --   end,
+  -- },
 
   -- neta
   { 'seandewar/nvimesweeper' },
