2120cdec3cd3a7a246fcd9a19e592cde909e6eaa
yahomi-dev <yahomi.dev@gmail.com>
2024-09-06 14:33:42 +0900

    lua/config修正

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -1,8 +1,1 @@
-return {
-  vim.api.nvim_create_autocmd({ 'CursorHold' }, {
-    pattern = { '*' },
-    callback = function()
-      require('lspsaga.diagnostic').show_cursor_diagnostics()
-    end,
-  }),
-}
+return {}
