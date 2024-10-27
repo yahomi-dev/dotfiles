d6ba3122155ff673f097fd2df8fc3bd07fb68507
yahomi-dev <yahomi.dev@gmail.com>
2024-10-28 01:39:16 +0900

    vscode設定

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,8 +6,13 @@ end
 -- すべてのオートコマンドをクリア
 vim.cmd('autocmd!')
 
--- wsl用コピペ設定
-vim.cmd([[
+if vim.g.vscode then
+  -- VS Code用の設定ディレクトリにあるinit.luaを読み込み
+  vim.opt.rtp:prepend(vim.fn.stdpath('config') .. '/vscode_config')
+  dofile(vim.fn.stdpath('config') .. '/vscode_config/init.lua')
+else
+  -- wsl用コピペ設定
+  vim.cmd([[
   if system('uname -a | grep microsoft') != ''
     augroup myYank
       autocmd!
@@ -16,18 +21,19 @@ vim.cmd([[
   endif
 ]])
 
-require('config.options')
+  require('config.options')
 
-require('config.keymaps')
+  require('config.keymaps')
 
-require('config.lazy')
+  require('config.lazy')
 
--- colorScheme
--- vim.cmd.colorscheme('habamax')
--- vim.cmd.colorscheme('ayu')
-vim.cmd.colorscheme('catppuccin')
--- vim.cmd.colorscheme('everforest')
+  -- colorScheme
+  -- vim.cmd.colorscheme('habamax')
+  -- vim.cmd.colorscheme('ayu')
+  vim.cmd.colorscheme('catppuccin')
+  -- vim.cmd.colorscheme('everforest')
 
-vim.cmd([[
+  vim.cmd([[
   :autocmd InsertLeave * :silent !/opt/homebrew/bin/im-select com.apple.keylayout.ABC
 ]])
+end
